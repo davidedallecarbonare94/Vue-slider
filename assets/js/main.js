d7a984635b29421147ab09d002fdabfbe5323e5b
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-05-18 16:06:56 +0200

    no solution found

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,24 +9,22 @@ let app = new Vue({
                     "https://images-na.ssl-images-amazon.com/images/I/81fwIFkvLGL._AC_SL1500_.jpg",
                     "https://images-na.ssl-images-amazon.com/images/I/81V1Ph3IQIL._AC_SY679_.jpg",
                     "https://images-na.ssl-images-amazon.com/images/I/81N8HnjtMhL._AC_SL1414_.jpg"
-                ]
-
+                ],
+                button: "fas fa-circle"
             },
             methods: {
                 prev() {
-                    console.log("ho clicckato qua");
                     if (this.counter === 0) {
                         return this.counter = this.images.length - 1
                     }
                     return this.counter -= 1
                 },
                 next() {
-                    console.log("ho cliccato qui");
                     if (this.counter === this.images.length - 1) {
                         return this.counter = 0
                     }
                     return this.counter += 1
-                },
-
-            }
+                }
+    },
 })
+
